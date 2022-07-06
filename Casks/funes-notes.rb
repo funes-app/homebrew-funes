ddd773026335e2119bebed93185c82eec3b825cd
Justin LeBlanc <leblanc@wustl.edu>
2022-07-06 14:35:23 -0500

    Add cask for Funes Notes

---
+++ Casks/funes-notes.rb
@@ -0,0 +1,15 @@
+                                   formula.
+      --tap                        Generate the new formula within the given
+                                   tap, specified as user/repo.
+  -f, --force                      Ignore errors for disallowed formula names
+cask "funes-notes" do
+  version "1.0.1"
+  sha256 "8b0810d7ef44d214af7018bbb00990a0e6368b7bcb965f8dc61066114b7d2cf6"
+
+  url "https://github.com/funes-app/FunesNotes/releases/download/v#{version}/FunesNotes.app.zip"
+  name "Funes Notes"
+  desc "App for taking notes and backing them up on your Urbit ship"
+  homepage "https://github.com/funes-app"
+
+  app "FunesNotes.app"
+end
